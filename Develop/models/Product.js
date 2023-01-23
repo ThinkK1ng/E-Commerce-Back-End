2ab8a20120c8d2222028e04d160f9717c2110ee5
ThinkK1ng <cla.givens@gmail.com>
2023-01-23 14:07:49 -0600

    added code to module files and category-routes.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNUll: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNUmeric: true
+      }
+    }, category_id: {
+      type: DataTypes.INTERGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
